6d129e4852f479e037234f29b9a679d383f565d0
sqrtthree <sqrtthree@foxmail.com>
2016-06-28 23:20:47 +0800

    Site updated: 2016-06-28 23:20:46

---
+++ dist/js/index.js
@@ -0,0 +1,67 @@
+(function() {
+  var async;
+
+  async = function(url, callback) {
+    var iT, oScript;
+    iT = 'script';
+    oScript = document.createElement(iT);
+    oScript.src = url;
+    if (callback) {
+      oScript.addEventListener('load', function(event) {
+        return callback(null, event);
+      }, false);
+    }
+    return document.body.appendChild(oScript);
+  };
+
+  (function() {
+    if ($('#tag_cloud').length) {
+      return async('/dist/js/jquery.tagcloud.js', function() {
+        $.fn.tagcloud.defaults = {
+          color: {
+            start: '#bbbbee',
+            end: '#0085a1'
+          }
+        };
+        return $('#tag_cloud a').tagcloud();
+      });
+    }
+  })();
+
+  (function() {
+    if ($('.toc-nav-button').length) {
+      return $('.toc-nav-button').click(function() {
+        $(this).html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
+        return async('/dist/js/toc.min.js');
+      });
+    }
+  })();
+
+  $(function() {
+    var MQL, headerHeight;
+    MQL = 1170;
+    if ($(window).width() > MQL) {
+      headerHeight = $('.navbar-custom').height();
+      return $(window).on('scroll', {
+        previousTop: 0
+      }, function() {
+        var currentTop;
+        currentTop = $(window).scrollTop();
+        if (currentTop < this.previousTop) {
+          if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+            $('.navbar-custom').addClass('is-visible');
+          } else {
+            $('.navbar-custom').removeClass('is-visible is-fixed');
+          }
+        } else {
+          $('.navbar-custom').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+            $('.navbar-custom').addClass('is-fixed');
+          }
+        }
+        return this.previousTop = currentTop;
+      });
+    }
+  });
+
+}).call(this);
---
+++ src/js/toc.coffee
@@ -0,0 +1,18 @@
+$sideToc = $('<div>', { class: 'col-md-3'} )
+html = "
+      <div class=\"toc-box affix\">
+        <div class=\"toc-heading\">文章目录</div>
+        <div class=\"toc-body\">
+          <div class=\"toc-cont\">
+            <div class=\"highlight-title\"></div>
+          </div>
+        </div>
+      </div>
+      "
+
+$sideToc.html(html)
+
+$('#markdown-container')
+  .addClass('col-md-9')
+  .removeClass('col-md-10 col-md-offset-1')
+  .after($sideToc)
